5e0a1ccc215878a0df35d4c3f687f7c51dc1dc7b
BahaaJber1 <bahaajber23@gmail.com>
2025-08-14 18:33:19 +0300

    Created the layout

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,6 @@ import Navigation from "../components/Navigation";
 export default function Page() {
     return (
         <div>
-            <Navigation />
             <h1> About the wild oasis</h1>
         </div>
     );
