adb99b887053392185723e8db7cc6f168484555a
BahaaJber1 <bahaajber23@gmail.com>
2025-09-29 19:37:21 +0300

    Enhance UX with Suspense with a spinner fallback & connect the project to supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	// To allow images from Supabase storage
+	images: {
+		remotePatterns: [
+			new URL(
+				"https://jbdewevkmmiopkqgpufd.supabase.co/storage/v1/object/public/cabin-images/**"
+			),
+		],
+	},
+};
 
 export default nextConfig;
