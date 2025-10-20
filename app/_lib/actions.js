afbc1c386a9bde9400e20cbcaafe14a6b82059e4
BahaaJber1 <bahaajber23@gmail.com>
2025-10-20 20:36:56 +0300

    Build the delete reservations page, create server action for that and make sure the user is authenticated/authoraized to perform the deleting, add loading indicator using the useTransation hook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "@lib/auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
 	// for using the formData API to get form values we need to specify the name attribute on the form fields
@@ -28,12 +29,39 @@ export async function updateGuest(formData) {
 		.from("guests")
 		.update(updateData)
 		.eq("id", session.user.guestId);
-	
+
 	if (error) {
 		throw new Error("Guest could not be updated");
 	}
 
 	revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+	const session = await auth();
+
+	if (!session) {
+		throw new Error("You must be signed in to perform this action");
+	}
+
+	const guestBookings = await getBookings(session.user.guestId);
+
+	const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+	if (!guestBookingIds.includes(bookingId)) {
+		throw new Error("You are not authorized to delete this reservation");
+	}
+
+	const { error } = await supabase
+		.from("bookings")
+		.delete()
+		.eq("id", bookingId);
+
+	if (error) {
+		throw new Error("Booking could not be deleted");
+	}
+
+	revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
